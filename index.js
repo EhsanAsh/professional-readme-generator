b4557e1d751eb9089f9512975b73fba8abd1de4d
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-04 16:06:32 -0400

    feat(massage): add first initial massage to start the application

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./assets/generateMarkdown.js");
 const colors = require("colors");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+console.log("Hi, this application is created to generate a professional README for your projects.".rainbow);
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
