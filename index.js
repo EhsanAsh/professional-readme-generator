7696de4abae36e7988ed5c7b2d0eff043fdd01d0
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-05 01:22:18 -0400

    build(path): create a folderPath to locate the readme file when its created

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./assets/generateMarkdown.js");
+// Importins colors module to add colors to the console.log
 const colors = require("colors");
+const folderPath = process.argv[2] || process.cwd() + "/README.md";
 
 console.log(colors.rainbow(
   `Hi, this application is created to generate a professional README for your projects.
-  You will be asked to answer some questions and enter som information to create your README file as you are expecting`
+  You will be asked to answer some questions and enter information to create your README file as expected.`
 ));
 
 // array of questions for user
@@ -25,16 +27,16 @@ inquirer
   .prompt(questions)
 
   .then(answers => {
-    console.log(colors.cyan("Your answers: ", answers));
+    console.log(colors.cyan("Your answer is: ", answers));
     const readmecontent = generateMarkdown(answers);
-    writeToFile("./assets/README.md", readmecontent);
+    writeToFile(folderPath, readmecontent);
   })
   .catch(err => console.log("An Error occured:", err));
 
 // function to write README file
 const writeToFile = (fileName, data) => {
   fs.writeFile(fileName, data, (err) => {
-    err ? console.log(err) : console.log("README file created successfully");
+    err ? console.log(err) : console.log(colors.red("README file created successfully"));
   });
 };
 
