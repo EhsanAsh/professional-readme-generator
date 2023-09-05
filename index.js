296635a3838d82cc9776c512ed47aaf198e72ef5
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-05 17:00:20 -0400

    feat(prompt): add a new question related to description- add new inquirer class

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const generateMarkdown = require("./assets/generateMarkdown.js");
 // Importins colors module to add colors to the console.log
 const colors = require("colors");
 const folderPath = process.argv[2] || process.cwd() + "/README.md";
+// Creating a new instance of the inquirer.ui.BottomBar class
+const ui = new inquirer.ui.BottomBar();
+ui.log.write(colors.brightGreen.bold("Generating README file..."));
 
 console.log(colors.rainbow(
   `Hi, this application is created to generate a professional README for your projects.
-  You will be asked to answer some questions and enter information to create your README file as expected.`
+You will be asked to answer some questions and enter information to create your README file as expected.
+`
 ));
 
 // array of questions for user
@@ -16,8 +20,15 @@ const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the title of your project?",
-    validate: (value) => value ? true : "Please enter a valid title for your project",
+    message: colors.green.bold("What is the title of your project?"),
+    validate: (value) => value ? true : colors.red.bold("Please enter a valid title for your project"),
+  },
+  {
+    type: "editor",
+    name: "description",
+    message: colors.green.bold("Please enter a description for your project:"),
+    validate: (value) => value ? true : colors.red.bold("Please enter a valid description for your project"),
+    default: colors.yellow.bold("Add your desired description here"),
   }
 
 ];
@@ -27,19 +38,18 @@ inquirer
   .prompt(questions)
 
   .then(answers => {
-    console.log(colors.cyan("Your answer is: ", answers));
     const readmecontent = generateMarkdown(answers);
     writeToFile(folderPath, readmecontent);
   })
-  .catch(err => console.log("An Error occured:", err));
-
-// function to write README file
-const writeToFile = (fileName, data) => {
-  fs.writeFile(fileName, data, (err) => {
-    err ? console.log(err) : console.log(colors.red("README file created successfully"));
-  });
-};
-
+  .catch(err => console.log(colors.red.bold("An Error occured:"), err));
+  
+  // function to write README file
+  const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+      err ? console.log(err) : console.log(colors.brightBlue.bold("README file created successfully"));
+    });
+  };
+  
 // TODO: Create a function to initialize app
 function init() {}
 
