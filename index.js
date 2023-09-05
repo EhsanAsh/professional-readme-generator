c5bbfd48eeb6ba0de8508ff052a3fa95f36e2bf4
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-04 23:14:19 -0400

    feat(title): create first input for the projects Title

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const fs = require("fs");
 const generateMarkdown = require("./assets/generateMarkdown.js");
 const colors = require("colors");
 
-console.log("Hi, this application is created to generate a professional README for your projects.".rainbow);
+console.log(`Hi, this application is created to generate a professional README for your projects.
+You will be asked to answer some questions and enter som information to create your README file as you are expecting`.rainbow);
 
+// array of questions for user
+const questions = [
 
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: (value) => value ? true : "Please enter a valid title for your project",
+  }
+
+];
+
+inquirer
+  
+  .prompt(questions)
+
+  .then(answers => {
+    console.log(answers);
+    const readmecontent = generateMarkdown(answers);
+    fs.writeFile("./assets/README.md", readmecontent, (err) => {
+      err ? console.log(err) : console.log("README file created successfully");
+    })
+      
+  .catch(err => console.log("An Error occured:", err));
+    
+  });
 
 
 // TODO: Create a function to write README file
