e7a729cc9e0599fa02f84b1bb3da853325e96a69
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-05 22:42:01 -0400

    feat(license): add two functions in generateMarkdown.js to render license link and section

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,26 +1,33 @@
 // a function that returns a license badge based on which license is passed in
 const renderLicenseBadge = (license) => {
   if (license !== "None") {
-    return `This project is licensed under the ${license} license.
-    ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg) | [![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
+    return `${renderLicenseLink} ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg) | [![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
   }
   return '';
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Creating a function that returns the license link
+const renderLicenseLink = (license) => {
+  if (license !== "None") {
+    return `* [License](#license)`;
+  }
+  return '';
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Creating a function that returns the license section of README
+const renderLicenseSection = (license) => {
+  if (license !== "None") {
+    return `## License
+    This project is licensed under the ${license} license.`;
+  }
+  return '';
+};
 
-// TODO: Create a function to generate markdown for README
+// Creating a function to generate markdown for README
 function generateMarkdown(data) {
 
   return `# ${data.title}
 
-## License
 ${renderLicenseBadge(data.license)}
 
 ## Description
@@ -40,6 +47,10 @@ ${data.installation}
 
 ## Credits
 ${data.credits}
+
+---
+
+${renderLicenseSection(data.license)}
 
 ---
 
